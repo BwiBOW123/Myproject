ab1e14dd462daba89c5b0026d3c5518b13cdbeff
BwiBOW123 <thanawatkheawkajee@gmail.com>
2024-05-17 11:06:00 +0700

    SetUp

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { DwtComponent } from './dwt/dwt.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,TestComponent,DwtComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'myproject';
+}
---
+++ src/app/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestComponent } from '../test/test.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TestComponent
+  ]
+})
+export class AppModule { }
